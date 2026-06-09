8ecaf06ebdbf69f9ef9e14c76064c14e66913853
Deeptangshu Mazumder <imazdeep@MacBookAir.communityfibre.co.uk>
2026-06-09 02:50:29 +0100

    Revert "Refresh UI and add weather icons"
    This reverts commit 9f7420a82b1e85239f65dbe9a33f6df3890f8afb.

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Layout from './components/Layout';
-import VatsimMetarPage from './pages/VatsimMetarPage';
-import NotFound from './pages/NotFound';
 import './styles.css';
 
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<App />} />
-            <Route path="/vatsim" element={<VatsimMetarPage />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <App />
     </React.StrictMode>
   );
 }
